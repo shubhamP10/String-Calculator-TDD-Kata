2a1ee0bcf814dc3b148cd0e0ee54dd6d4fd203c9
shubhamP10 <shubhampattar2@gmail.com>
2022-03-10 21:05:09 +0530

    Updated: Code to Avoid Failures

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,19 +1,20 @@
 public class StringCalculator {
     public int add(String numbers) {
         int result = 0;
-        String delimiter = "";
-        StringBuffer nums = new StringBuffer(numbers);
-
-        if (numbers.startsWith("//")){
-            delimiter = String.valueOf(numbers.charAt(2));
-            numbers = nums.substring(3,nums.length());
-        }
-
-        String[] numbersArray = numbers.split(delimiter+"|,|\n");
 
         if (numbers.equals(""))
             return 0;
         else {
+            String delimiter = null;
+            StringBuffer nums = new StringBuffer(numbers);
+
+            if (numbers.startsWith("//")){
+                delimiter = String.valueOf(numbers.charAt(2));
+                numbers = nums.substring(3,nums.length());
+            }
+
+            String[] numbersArray = numbers.split(",|\\\n|"+delimiter);
+
             for (String number : numbersArray) {
                 result += Integer.parseInt(number);
             }
