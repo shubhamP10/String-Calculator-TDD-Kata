f7f7587b9ca3fa42f7ff83b54c3f3b23dbf1cc42
shubhamP10 <shubhampattar2@gmail.com>
2022-03-10 20:21:14 +0530

    Added: Test Case to Validate Add Method With 1 Number

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,7 +1,16 @@
 public class StringCalculator {
     public int add(String numbers) {
+        int result = 0;
+        String[] numbersArray = numbers.split(",");
+
         if (numbers.equals(""))
             return 0;
-        return -1;
+        else {
+            for (String number : numbersArray) {
+                result += Integer.parseInt(number);
+            }
+        }
+
+        return result;
     }
 }
--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 public class StringCalculatorTest {
 
     /*
-        * Test case to validate object creation
+    * Test case to validate object creation
     */
     @Test
     public void testCalculatorConstructor() {
@@ -12,10 +12,23 @@ public class StringCalculatorTest {
         Assert.assertNotNull(calculator);
     }
 
+    /*
+     * Test case to validate Add method with empty string
+     */
     @Test
     public void testCalculatorMethodWithEmptyString() {
         StringCalculator calculator = new StringCalculator();
         int result = calculator.add("");
         Assert.assertEquals(0, result);
+    }
+
+    /*
+     * Test case to validate Add method with 1 number
+     */
+    @Test
+    public void testCalculatorMethodWithOneNumber() {
+        StringCalculator calculator = new StringCalculator();
+        int result = calculator.add("1");
+        Assert.assertEquals(1, result);
     }
 }
