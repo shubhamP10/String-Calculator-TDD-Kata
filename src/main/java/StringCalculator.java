88b350e9fcf5b5011a4f89c64275caf8d5e9436b
shubhamP10 <shubhampattar2@gmail.com>
2022-03-10 20:59:34 +0530

    Added: Test Case to Validate Add Method Custom Delimiter

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,7 +1,15 @@
 public class StringCalculator {
     public int add(String numbers) {
         int result = 0;
-        String[] numbersArray = numbers.split(",|\\\n");
+        String delimiter = "";
+        StringBuffer nums = new StringBuffer(numbers);
+
+        if (numbers.startsWith("//")){
+            delimiter = String.valueOf(numbers.charAt(2));
+            numbers = nums.substring(3,nums.length());
+        }
+
+        String[] numbersArray = numbers.split(delimiter+"|,|\n");
 
         if (numbers.equals(""))
             return 0;
